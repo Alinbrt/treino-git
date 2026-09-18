b25f13de3aa628a2e1345442d2dee0e65d0b46d3
Alinbrt <aline.torresbrt188@ufu.br>
2026-09-18 19:47:42 -0300

    feat: implementar modulo basico de autenticacao

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 print('Versao da funcionalidade A')
 print('Versao da funcionalidade B')
-print('Autenticacao e seguranca ativadas.')print('Autenticacao e seguranca ativadas.')
+print('Autenticacao e seguranca ativadas.')
